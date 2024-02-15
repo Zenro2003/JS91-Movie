7955435d5659bcfdc6df2df923dacde094c743b8
Vu Huy <vulehuy0102@gmail.com>
2024-02-15 20:35:04 +0700

    complete

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -36,6 +36,19 @@ const Footer = () => {
               <Link to="/">Top IMDB</Link>
           </div>
         </div>
+        <div className="footer__content__map">
+          {/* Google Maps iframe */}
+          <iframe
+            title="Google Map"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.497872789132!2d106.69061257480482!3d10.773128089375424!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752f3c0189fa2b%3A0x6e75dc36d4dba07d!2sGalaxy%20Nguy%E1%BB%85n%20Du!5e0!3m2!1svi!2s!4v1706970173350!5m2!1svi!2s"
+            width="600"
+            height="450"
+            className="google-map"
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
       </div>
     </div>
   );
